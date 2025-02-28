ef87e55f74f9ed9e8bae1a85cacc858790b55aea
davitmetreveli-hub <iamwantedguy@gmail.com>
2025-02-28 22:19:44 +0400

    Davit_Metreveli123.py

--- Davit_Metreveli123.py
+++ Davit_Metreveli123.py
@@ -1,24 +1,18 @@
-class Bank_Account:
-    def __init__(self, account_name, balance=0):
-        self.accountname = account_name
-        self.balance = balance
-    def getter(self):
-        return self.__account_name
-    def setter(self, name):
-        self.accountname = name
-    def deposit(self,
 class Bankaccount:
-    def __init__(self, accountname, balance=0):
-        self.accountname = accountname
-        self.balance = balance
+    def __init__(self,accountname,balance=0):
+        self.__accountname = accountname
+        self.__balance = balance
     def getaccname(self):
         return self.__accountname
-    def setaccname(self, name):
-        self.__accountname = name
-    def deposit(self, amount):
+    def getbalance(self):
+        return self.__balance
+    def setaccountname(self,newaccountname):
+        self.__accountname = newaccountname
+    def setbalance(self,newbalance):
+        self.__balance = newbalance
+    def deposit(self,amount):
         self.__balance += amount
-        print(f"sucessful deposit ow you have {self.balance}")
-    def undeposit(self, amount):
-        if amount <= self.balance:
-            self.balance = balance - amount
-            print(f"undeposit succesfuk now you have {self.__balance}")
+        print(f"თანხა შეტანილია ახლა თქვენ გაქვთ{self.__balance} ლარი")
+    def withdraw(self, amount):
+        self.__balance -= amount
+        print(f"თანხა გამოტანილია თქვენ გაქვთ{self.balance} ლარი")
